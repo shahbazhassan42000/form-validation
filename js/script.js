78fa164a5394bf0b3efde86d4f8d104a887926af
Shahbaz Hassan <shahbazhassan42000@gmail.com>
2021-09-01 01:47:34 +0500

    on invalid state show message in p tag after input element and hide on being in valid state

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",function (){
     const patterns={
         username:/^[a-z\d]{5,12}$/i,
-        email:/^\w+@\w+.com$/,
+        email:/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
         password:/^[\w-@]{8,20}$/,
         telephone:/^\d{12}$/,
         slug:/^[a-z0-9-]{8,20}$/
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded",function (){
     document.addEventListener("keyup",function (event) {
         if((event.target.tagName)=="INPUT") //check if input filed is triggered
         {
-
             validate(event.target,patterns[event.target.attributes.name.value]);
         }
     });
@@ -17,11 +16,33 @@ document.addEventListener("DOMContentLoaded",function (){
 
         if(regex.test(inputField.value))
         {
-            inputField.style.borderColor="green";
+            inputField.style.borderColor="#36cc36";
+            if(inputField.attributes.name.value==="password")
+            {
+                let btn=document.querySelector("#show_hide_password_btn");
+                btn.nextElementSibling.style.opacity = "0";
+                btn.nextElementSibling.style.height = "0";
+
+            }
+            else {
+                inputField.nextElementSibling.style.opacity = "0";
+                inputField.nextElementSibling.style.height = "0";
+            }
         }
         else
         {
-            inputField.style.borderColor="red";
+            inputField.style.borderColor="orange";
+           if(inputField.attributes.name.value==="password"){
+               let btn=document.querySelector("#show_hide_password_btn");
+               btn.nextElementSibling.style.opacity="1";
+               btn.nextElementSibling.style.height="auto";
+               btn.nextElementSibling.style.marginBottom="20px";
+           }
+           else{
+               inputField.nextElementSibling.style.opacity="1";
+               inputField.nextElementSibling.style.height="auto";
+               inputField.nextElementSibling.style.marginBottom="20px";
+           }
         }
     }
     document.querySelector("#show_hide_password_btn").addEventListener('click',function (event) {
